3b47d0e5a892def9213ee483c1f592021b97c312
Johan-Mi <johanmi@protonmail.com>
2020-11-24 18:56:30 +0100

    -

--- src/types/error.rs
+++ src/types/error.rs
@@ -1,17 +1,7 @@
-use std::fmt;
+use derive_more::{Constructor, Display};
 
+#[derive(Display, Constructor)]
+#[display(fmt = "Error: {}", message)]
 pub struct Error {
     message: String,
-}
-
-impl fmt::Display for Error {
-    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-        write!(formatter, "Error: {}", self.message)
-    }
-}
-
-impl Error {
-    pub fn new(message: String) -> Self {
-        Self { message }
-    }
 }
